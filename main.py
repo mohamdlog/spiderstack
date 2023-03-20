579c2be4fc58b48f63d55c7e1c7debcdafc8d1e2
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2023-03-20 02:35:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
+import stack
 import scrapy
-from pathlib import Path
 
-permission = input("Would you like to create a new folder for data_fetcher? (y/n)\n")
-if permission == "y":
-    newpath = input("Enter directory for new data_fetcher folder:\n") + "data_fetcher"
+
+permission = input("Do you have an existing output folder for data_fetcher? (y || n)\n")
+if permission == "n":
+    newpath = input("Enter directory for new output folder for data_fetcher:\n") + "data_fetcher"
     if not os.path.exists(newpath):
         os.makedirs(newpath)
+
+data = input("What type of data would you like?\n( quotes || books || )\n")
+
+match data:
+    case 'quotes':
+        os.system("scrapy crawl quotes")
+    case _:
+        print("Invalid option, try again.")
