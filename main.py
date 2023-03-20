fa6eeeb09fd5ec07978195b5e2aad57579f26ecc
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2023-03-20 04:47:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import os
+from pathlib import Path
+import shutil
+import glob
 import stack
+import os
 
 
 path = input("Enter directory for new data_fetcher folder:\n") + "data_fetcher"
-if not os.path.exists(path):
-    os.makedirs(path)
 if os.path.exists(path):
     print("Existing data_fetcher folder found, moving on.\n")
+if not os.path.exists(path):
+    os.makedirs(path)
 
 data = input("What type of data would you like?\n( quotes || books || )\n")
 
