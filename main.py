bd35f4a7400bf6480c5dfb67d9b5bd2daa7ceebb
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2023-03-24 17:08:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 path = None
-create = input("Create folder to store data? (y/n) ")
+create = input("Create folder to store data? (y/n)\n")
 if create == 'y':
     path = input("\nEnter path for new folder:\n") + "scraped_data/"
     if os.path.exists(path):
@@ -14,20 +14,17 @@ if create == 'y':
         os.makedirs(path)
 
 data = input("\nSelect subject to start scraping.\n( quotes || wikipedia || )\n")
-ext = None
 
 match data:
     case 'quotes':
         os.system("scrapy crawl quotes -O quotes.json")
-        ext = '\*.json'
     case 'wikipedia':
-        os.system("scrapy crawl wikipedia")
-        ext = ''
+        os.system("scrapy crawl wikipedia -O wiki.json")
     case _:
         print("Invalid option, try again.")
 
 if path is not None:
-    files = glob.iglob(f'{Path()}{ext}')
+    files = glob.iglob(f'{Path()}/*.json')
     for file in files:
         file_name = os.path.basename(file)
         shutil.move(file, f'{path}{file_name}')
