c9eb1c90361ea8b3e7e1df886bb53a2d8c2f30a2
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2023-03-24 16:08:27 -0500

    Update quotes_spider.py

--- stack/spiders/quotes_spider.py
+++ stack/spiders/quotes_spider.py
@@ -4,22 +4,24 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = []
-    page_amount = int(input("\nEnter amount of pages to scrape:\n"))
-    page_strict = set() if input("\nAllow duplicates? (y/n) ") == 'n' else 0
 
-    for i in range(page_amount):
-        page_number = 0
+    def __init__(self):
+        page_amount = int(input("\nEnter amount of pages to scrape:\n"))
+        page_strict = set() if input("\nAllow duplicates? (y/n)\n") == 'n' else 0
 
-        while page_number < 1 or page_number > 10:
-            page_number = int(input("\nEnter a page number to scrape: (1 - 10)\n"))
-            if page_strict != 0 and page_number in page_strict:
-                print("Page number already scraped, try again.")
-                page_number = 0
-                continue
-        else:
-            start_urls.append(f'https://quotes.toscrape.com/page/{page_number}')
-            page_strict.add(page_number) if page_strict != 0 else None
-    
+        for i in range(page_amount):
+            page_number = 0
+
+            while page_number < 1 or page_number > 10:
+                page_number = int(input("\nEnter a page number to scrape: (1 - 10)\n"))
+                if page_strict != 0 and page_number in page_strict:
+                    print("Page number already scraped, try again.")
+                    page_number = 0
+                    continue
+            else:
+                self.start_urls.append(f'https://quotes.toscrape.com/page/{page_number}')
+                page_strict.add(page_number) if page_strict != 0 else None
+  
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {
