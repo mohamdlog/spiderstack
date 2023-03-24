fd31d2fcb389cfabdf6d56bd98db911de476b6ea
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2023-03-24 14:25:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import glob
 import os
 from pathlib import Path
 
+path = None
+create = input("Create folder to store data? (y/n) ")
+if create == 'y':
+    path = input("\nEnter path for new folder:\n") + "scraped_data/"
+    if os.path.exists(path):
+        print("Existing data_fetcher folder found, continuing.")
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-path = input("Enter directory for new data_fetcher folder:\n") + "data_fetcher/"
-if os.path.exists(path):
-    print("Existing data_fetcher folder found, continuing.\n")
-if not os.path.exists(path):
-    os.makedirs(path)
-
-data = input("What subject would you like data of?\n( quotes ||  ||  || )\n")
+data = input("\nSelect subject to start scraping.\n( quotes || || )\n")
 ext = None
 
 match data:
@@ -20,7 +22,8 @@ match data:
     case _:
         print("Invalid option, try again.")
 
-files = glob.iglob(f'{Path()}{ext}')
-for file in files:
-    file_name = os.path.basename(file)
-    shutil.move(file, f'{path}{file_name}')
+if path is not None:
+    files = glob.iglob(f'{Path()}{ext}')
+    for file in files:
+        file_name = os.path.basename(file)
+        shutil.move(file, f'{path}{file_name}')
