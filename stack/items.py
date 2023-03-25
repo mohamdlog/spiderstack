761af56279f431dfab235764a190b3080965a8ea
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2023-03-25 17:23:19 -0500

    Update items.py

--- stack/items.py
+++ stack/items.py
@@ -4,3 +4,14 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+
+class FinanceItem(scrapy.Item):
+    stock_name = scrapy.Field()
+    previous_close = scrapy.Field()
+    open = scrapy.Field()
+    bid = scrapy.Field()
+    ask = scrapy.Field()
+    range_day = scrapy.Field()
+    range_52weeks = scrapy.Field()
+    volume = scrapy.Field()
+    avg_volume = scrapy.Field()
