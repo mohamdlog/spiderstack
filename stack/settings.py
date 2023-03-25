577e04f9783df5164fb0bf48275cf02dc848bff0
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2023-03-25 17:23:24 -0500

    Update settings.py

--- stack/settings.py
+++ stack/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "stack.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "stack (+http://www.yourdomain.com)"
+USER_AGENT = "(http://www.yourdomain.com)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
